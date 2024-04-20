6ef58a15c145faa479af7bb83b5e1ac9548c707b
Endy1381 <119288346+Endy1381@users.noreply.github.com>
2024-04-20 19:42:20 +0300

    Update responsive 2

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function handleClick(){
+    let resp = document.querySelector('.ul-responsive')
+    if (resp.style.display === 'none') {
+        resp.style.display = 'block';
+    } else {
+        resp.style.display = 'none';
+    }
+}
