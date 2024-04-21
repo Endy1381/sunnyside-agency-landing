bb09a21420f8703117bde200650597792f1b98ba
Endy1381 <119288346+Endy1381@users.noreply.github.com>
2024-04-21 11:53:03 +0300

    Update
    tweaked the contact button

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
 function handleClick(){
     let resp = document.querySelector('.ul-responsive')
+    let norm = document.querySelector('.norm')
     if (resp.style.display === 'none') {
         resp.style.display = 'block';
+        norm.style.display = 'none'; // Changed from norm.style.type
     } else {
         resp.style.display = 'none';
+        norm.style.display = 'block'; // This line is correct
     }
 }
